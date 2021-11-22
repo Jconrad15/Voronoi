ed8d570375cc6eca92cd7ceaaab50f73c9d82135
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2021-11-22 13:33:46 -0800

    Movement and colliders

--- Voronoi/Assets/Scripts/PlayerController.cs
+++ Voronoi/Assets/Scripts/PlayerController.cs
@@ -7,31 +7,56 @@ namespace Voronoi
     public class PlayerController : MonoBehaviour
     {
         [SerializeField]
-        private float speed = 2000000f;
+        private CharacterController controller;
 
-        private Rigidbody rb;
+        [SerializeField]
+        private Transform cam;
 
-        // Start is called before the first frame update
-        void OnEnable()
+        public float moveSpeed = 4f;
+        public float jumpSpeed = 2f;
+
+        public float turnSmoothTime = 0.1f;
+        private float turnSmoothVelocity;
+
+        private float gravity = 9.8f;
+
+        private float vSpeed = 0;
+
+        private void Update()
         {
-            rb = GetComponent<Rigidbody>();
-        }
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-        void FixedUpdate()
-        {
-            float moveHorizontal = Input.GetAxis("Horizontal");
-            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-            if (moveHorizontal != 0 || moveVertical != 0)
+            if (direction.magnitude >= 0.05f || vSpeed != 0)
             {
-                Vector3 movement = ((transform.forward * moveVertical) +
-                                   (transform.right * moveVertical)).normalized;
+                // Rotate
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(
+                    transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
-                Debug.Log(movement.magnitude);
-                rb.AddForce(speed * movement);
+                transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+                Vector3 moveDir = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized;
+
+                // Jump and gravity
+                if (controller.isGrounded)
+                {
+                    vSpeed = 0; // grounded character has vSpeed = 0...
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    { // unless it jumps:
+                        vSpeed = jumpSpeed;
+                    }
+                }
+                // apply gravity acceleration to vertical speed:
+                vSpeed -= gravity * Time.deltaTime;
+                moveDir.y = vSpeed; // include vertical speed in vel
+
+                controller.Move(moveSpeed * Time.deltaTime * moveDir);
             }
-        }
 
+        }
 
     }
 }
